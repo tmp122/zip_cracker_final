d675ec8411d7706a30be002e318faa829ba17232
tmp122 <154687657+tmp122@users.noreply.github.com>
2023-12-24 12:47:47 +0530

    Add files via upload

---
+++ m1/script2.py
@@ -0,0 +1,38 @@
+import pyzipper
+from time import time
+ 
+def main():
+    path_zip=input("Enter path of the zip file: ")
+    path_pass=input("Enter path of the password file: ")
+    try:
+        with pyzipper.AESZipFile(path_zip) as myZip:
+            myZip.pwd=None
+    except pyzipper.BadZipfile:
+        print("[!] There was an error opening your zip file.")
+        return
+ 
+    password = ''
+ 
+    timeStart = time()
+    with open(path_pass, "r") as f:
+        passes = f.readlines()
+        for pass_count, x in enumerate(passes):
+            password = x.strip()
+            try:
+                with pyzipper.AESZipFile(path_zip) as myZip:
+                    myZip.pwd=password.encode('utf-8')
+                    myZip.extractall()
+                totalTime = time() - timeStart
+                print("\nPassword cracked: %s\n" % password)
+                print("%i password attempts per second." % (pass_count / totalTime))
+                return
+            except Exception as e:
+                print(e)
+                continue  # Continue to the next password
+        else:
+            print("Sorry, password not found.")
+ 
+if __name__ == '__main__':
+    main()
+
+
